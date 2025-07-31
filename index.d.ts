17071dc2077fa6d2a28d3291ced8a941c946e4a8
Alex Schwartz <alexschwartz01@gmail.com>
2025-07-31 14:09:02 -0400

    fix typo

--- index.d.ts
+++ index.d.ts
@@ -8,7 +8,7 @@ declare function _exports({
   publicDir: string;
   port: number;
   hostname?: string;
-  routes? Record<string, ((data: any) => Promise<string>) | string>;
+  routes?: Record<string, ((data: any) => Promise<string>) | string>;
   httpsOptions?: {
     key: string;
     cert: string;
